30705e9af11233d18fc3a45383187ed026d4f5a5
Karan Sinha <karan.sinha1206@gmail.com>
2021-07-02 10:25:31 +0530

    login and signup backend

---
+++ ecommerce-back-end/src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const env = require('dotenv')
+const app = express();
+const mongoose = require('mongoose')
+
+const authRoutes = require('./routes/auth')
+
+env.config();
+mongoose.connect('mongodb://localhost:27017/test', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log('db connected');
+}).catch(err => {
+    console.log('there is an error in db connection');
+})
+
+
+
+app.use(express.json());
+app.use('/api', authRoutes);
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+})
---
+++ ecommerce-back-end/src/routes/auth.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const { signup, signin, requiresignin } = require('../controllers/auth');
+const router = express.Router();
+
+
+
+
+
+
+router.post('/signin', signin)
+
+router.post('/signup', signup)
+
+router.post('/profile',requiresignin , (req, res) => {
+    res.json({"user": "profile"})
+})
+
+module.exports = router
