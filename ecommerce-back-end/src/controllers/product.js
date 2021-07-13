a20dfb11c68ebb9dc1c8bfe827590482f137b38b
Karan Sinha <karan.sinha.1206@gmail.com>
2021-07-13 12:08:14 +0530

    upload multiple files in backend

--- ecommerce-back-end/src/controllers/product.js
+++ ecommerce-back-end/src/controllers/product.js
@@ -5,5 +5,5 @@ const shortid = require('shortid')
 exports.createProduct = (req, res) => {
 
     // const {  } = req.body;
-    res.status(200).json({file: req.file, body: req.body})
+    res.status(200).json({file: req.files, body: req.body})
 }
