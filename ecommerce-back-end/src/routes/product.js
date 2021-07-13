b8db02fc13d168fd3e4a43fe661ab2bc0a87f470
Karan Sinha <karan.sinha.1206@gmail.com>
2021-07-13 11:58:21 +0530

    minor changes

--- ecommerce-back-end/src/routes/product.js
+++ ecommerce-back-end/src/routes/product.js
@@ -3,7 +3,23 @@ const router = express.Router();
 const { requiresignin, adminMiddleware } = require('../common-middleware');
 const { createProduct } = require('../controllers/product');
 const multer = require('multer');
-const upload = multer({dest: 'uploads/'})
+const shortid = require('shortid');
+const path = require('path');
+
+
+
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.join(path.dirname(__dirname), 'uploads'))
+    },
+    filename: (req, file, cb) =>{
+        cb(null, shortid.generate() + "-" + file.originalname);
+    }
+})
+
+
+const upload = multer({storage})
 
 
 
